cb137f2c17441c05a7fdc538c9800081b18acfb3
SamuelStrand <emil.vakhitov@list.ru>
2025-03-20 13:55:40 +0500

    uploaded lab8 backend

--- lab6/labka/src/app/album-detail/album-detail.component.ts
+++ lab6/labka/src/app/album-detail/album-detail.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
-import { ALBUMS } from '../fake-db';
-import { Albums } from '../models';
-import { AlbumService } from '../album.service';
 import { CommonModule } from '@angular/common';
+import { AlbumService } from '../album.service';
+import { Albums } from '../models';
 
 @Component({
   selector: 'app-album-detail',
@@ -12,29 +11,52 @@ import { CommonModule } from '@angular/common';
   templateUrl: './album-detail.component.html',
   styleUrl: './album-detail.component.css'
 })
-export class AlbumDetailComponent implements OnInit{
+export class AlbumDetailComponent implements OnInit {
 
   album!: Albums;
   loaded: boolean = false;
-  constructor(private route: ActivatedRoute, private albumService: AlbumService){
 
-  }
+  constructor(private route: ActivatedRoute, private albumService: AlbumService) {}
+
   ngOnInit(): void {
     this.getAlbum();
   }
 
-  getAlbum(){
+  getAlbum() {
     this.route.paramMap.subscribe((params) => {
       const albumId = Number(params.get('albumId'));
       this.loaded = false;
+
       this.albumService.getAlbum(albumId).subscribe((album) => {
         this.album = album;
         this.loaded = true;
-      })
-      //this.album = ALBUMS.find((album) => album.id === albumId) as Albums;
-
-    })
-
+      });
+    });
   }
 
+  updateTitle(event: Event) {
+    this.album.title = (event.target as HTMLInputElement).value;
+  }
+
+  updateBody(event: Event) {
+    this.album.body = (event.target as HTMLTextAreaElement).value;
+  }
+
+  saveAlbum() {
+    if (!this.album.title.trim()) {
+      alert('Title cannot be empty!');
+      return;
+    }
+
+    this.albumService.updateAlbum(this.album.id, this.album).subscribe(
+      (updatedAlbum) => {
+        console.log('Album updated:', updatedAlbum);
+        alert('Album updated successfully!');
+      },
+      (error) => {
+        console.error('Error updating album:', error);
+        alert('Failed to update album.');
+      }
+    );
+  }
 }
